6034dfd8659e738f839cf6421aaf5f168015a181
Aditeeniraula <aditeeniraula@gmail.com>
2024-12-09 21:27:58 +0545

    Probe

--- src/components/Dashboard/Probe.tsx
+++ src/components/Dashboard/Probe.tsx
@@ -1,21 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { httpBase } from "../../utils/axios.utils";
+import "react-toastify/dist/ReactToastify.css";
 
-function Probe() {
+const Probe = () => {
+  const [formData, setFormData] = useState({
+    max_life_time: "",
+    pool_size: "",
+    probe_factor: "",
+    probe_remove_factor: "",
+    mu: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await httpBase().post(
+        "/admin/update-prequal-parameters",
+        formData
+      );
+
+      if (response) {
+        toast.success("Replica added successfully!");
+        setFormData({
+          max_life_time: "",
+          pool_size: "",
+          probe_factor: "",
+          probe_remove_factor: "",
+          mu: "",
+        });
+      } else {
+        toast.error(
+          `Error: ${response.data.message || "Failed to add replica"}`
+        );
+      }
+    } catch (error: any) {
+      console.error("Error submitting the form:", error);
+      toast.error("An error occurred while adding the replica.");
+    }
+  };
+
   return (
     <div>
       <div className="bg-white shadow-md rounded-lg p-4 max-w-md mx-auto">
         <h2 className="text-lg font-medium mb-3">
           Probe to Reduce Latency Parameters
         </h2>
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={handleSubmit}>
           <div>
             <label className="block text-sm font-medium text-gray-700">
-              Max Life
+              Max Life Time
             </label>
             <input
               type="number"
+              name="max_life_time"
+              value={formData.max_life_time}
+              onChange={handleChange}
               className="mt-1 block w-full px-2 py-1 text-sm border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Enter max life"
+              placeholder="Enter max life time"
             />
           </div>
 
@@ -25,6 +72,9 @@ function Probe() {
             </label>
             <input
               type="number"
+              name="pool_size"
+              value={formData.pool_size}
+              onChange={handleChange}
               className="mt-1 block w-full px-2 py-1 text-sm border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
               placeholder="Enter pool size"
             />
@@ -36,6 +86,9 @@ function Probe() {
             </label>
             <input
               type="number"
+              name="probe_factor"
+              value={formData.probe_factor}
+              onChange={handleChange}
               className="mt-1 block w-full px-2 py-1 text-sm border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
               placeholder="Enter probe factor"
             />
@@ -47,6 +100,9 @@ function Probe() {
             </label>
             <input
               type="number"
+              name="probe_remove_factor"
+              value={formData.probe_remove_factor}
+              onChange={handleChange}
               className="mt-1 block w-full px-2 py-1 text-sm border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
               placeholder="Enter probe remove factor"
             />
@@ -58,6 +114,9 @@ function Probe() {
             </label>
             <input
               type="number"
+              name="mu"
+              value={formData.mu}
+              onChange={handleChange}
               className="mt-1 block w-full px-2 py-1 text-sm border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
               placeholder="Enter scale factor mu"
             />
@@ -70,9 +129,10 @@ function Probe() {
             Submit
           </button>
         </form>
+        <ToastContainer />
       </div>
     </div>
   );
-}
+};
 
 export default Probe;
